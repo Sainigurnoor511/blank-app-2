e362275afad40733a47e18561708a3dc59e27122
Sainigurnoor511 <sainigurnoor511@gmail.com>
2024-08-12 18:14:27 +0530

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import google.generativeai as genai
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Set the API key directly (replace 'YOUR_API_KEY' with your actual key)
+genai.configure(api_key='AIzaSyAl4kXbygumAaURklrsR8J6a5xGYy5cBAY')
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+
+st.title("Product Description Generator")
+st.write("This app generates a product description based on the product name and keywords you provide.")
+
+product_name = st.text_input("Enter product name:")
+keywords = st.text_input("Enter keywords (seperated by a comma):")
+
+response = model.generate_content(f"Write about {product_name} and use keywords {keywords}")
+print(response.text)
