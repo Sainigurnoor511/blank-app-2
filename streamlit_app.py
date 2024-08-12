9910151b736c98c261a3f9e2ec464141753273ba
Sainigurnoor511 <sainigurnoor511@gmail.com>
2024-08-12 22:57:34 +0530

    api: add a new endpoint

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,14 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-# Set the API key directly (replace 'YOUR_API_KEY' with your actual key)
-genai.configure(api_key='AIzaSyAl4kXbygumAaURklrsR8J6a5xGYy5cBAY')
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-
 st.title("Product Description Generator")
 st.write("This app generates a product description based on the product name and keywords you provide.")
+
+# Set the API key from an environment variable
+
+api_key = st.text_input("Enter your API key:")
+genai.configure(api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 product_name = st.text_input("Enter product name:")
 keywords = st.text_input("Enter keywords (seperated by a comma):")
